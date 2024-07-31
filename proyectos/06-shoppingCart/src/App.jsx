da8e5c3568d9f41f3496fdc77a04469af7626f59
StefanooV <stefano20151@gmail.com>
2024-07-31 13:11:39 -0300

    Correcion de filtro

--- proyectos/06-shoppingCart/src/App.jsx
+++ proyectos/06-shoppingCart/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <>
-      <Header/>
+      <Header changeFilters = {setFilter} />
       <Products products={filteredProducts} />
     </>
   )
--- proyectos/06-shoppingCart/src/components/Filters.jsx
+++ proyectos/06-shoppingCart/src/components/Filters.jsx
@@ -1,22 +1,42 @@
+import { useState } from 'react'
 import './Filters.css'
 
-export function Filters () {
+export function Filters ({ onChange }) {
+    const [minPrice, setMinPrice] = useState(0)
+
+    const handleChangeMinPrice = (evt) => {
+        setMinPrice(evt.target.value)
+        onChange(prevState => ({
+            ...prevState,
+            minPrice: evt.target.value
+        }))
+    }
+
+    const handleChangeCategory = (evt) => {
+        onChange(prevState => ({
+            ...prevState,
+            category: evt.target.value
+        }))
+    }
+
     return (
         <section className='filters'>
 
             <div>
-                <label htmlFor='price'>Price</label>
+                <label htmlFor='price'>Price from: </label>
                 <input 
                     type='range' 
                     id='price'
                     min='0'
                     max='1000'
+                    onChange={handleChangeMinPrice}
                 />
+                <span> ${minPrice} </span>
             </div>
 
             <div>
                 <label htmlFor='category'>Category</label>
-                <select id='category'>
+                <select id='category' onChange={handleChangeCategory}>
                     <option value='all'>All</option>
                     <option value='laptops'>Laptops</option>
                     <option value='smartphones'>Smartphones</option>
